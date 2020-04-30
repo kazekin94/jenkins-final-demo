76c638c4f36abf735076ac20fc474c909ebb1bfa
Ankit <ankit.thakur@juspay.in>
2020-05-01 03:51:48 +0530

    gg

--- build_scripts/build_script.py
+++ build_scripts/build_script.py
@@ -4,14 +4,20 @@ import docker
 
 
 #fetch parameters from ssm
-def fetch_parameter():
-    ssm_client=boto3.client('ssm')
+def fetch_parameter(para):
+    #set ssm client
+    ssm_client=boto3.client('ssm', region_name='ap-south-1')
+
     response = ssm_client.get_parameter(
-        Name='django-helloworld'
+        Name=para
     )
     print(response)
 
 
 if __name__ == "__main__":
-    docker_client=docker.from_env() 
-    ssm_fetch_para_response=fetch_parameter()
+    #env variables
+    para_name='django-helloworld'
+    #set docker client
+    docker_client=docker.from_env()
+    #calls 
+    ssm_fetch_para_response=fetch_parameter(para_name)
