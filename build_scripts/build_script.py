c84406758663b934e97071981019a78058d20803
Ankit <ankit.thakur@juspay.in>
2020-05-01 05:05:26 +0530

    added build image

--- build_scripts/build_script.py
+++ build_scripts/build_script.py
@@ -1,6 +1,7 @@
 import boto3
 import json
 import docker
+from io import BytesIO
 
 
 #fetch parameters from ssm
@@ -11,13 +12,27 @@ def fetch_parameter(para):
     response = ssm_client.get_parameter(
         Name=para
     )
-    print(response)
+    para_value=response['Parameter']['Value']
+    para_value_dict=json.loads(para_value)
+    return para_value_dict
 
+
+#build image
+def build_image(client, para, path_workspace):
+    print("Fetched para:", para)
+    actual_path=path_workspace.replace('<repo_name>', para['repo_name'])
+    docker_file=actual_path+"/Dockerfile"
+    print("Path to workspace:", docker_file)
+    image_build_response=client.images.build(path=docker_file)
+    print(image_build_response)
+    
 
 if __name__ == "__main__":
     #env variables
     para_name='django-helloworld'
+    work_space_path='/home/ec2-user/workspace/<repo_name>'
     #set docker client
     docker_client=docker.from_env()
     #calls 
-    ssm_fetch_para_response=fetch_parameter(para_name)
+    fetch_para_response=fetch_parameter(para_name) #fetch para
+    build_docker_image=build_image(docker_client, fetch_para_response, work_space_path) #build image
