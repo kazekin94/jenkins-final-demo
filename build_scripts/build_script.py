576b840c2b4288e1c76b48430989f388a51cd022
Ankit <ankit.thakur@juspay.in>
2020-05-01 03:42:35 +0530

    fetch para

--- build_scripts/build_script.py
+++ build_scripts/build_script.py
@@ -1,4 +1,16 @@
 import boto3
 import json
+import docker
 
-print("Hello world")
+
+#fetch parameters from ssm
+def fetch_parameter():
+    response = client.get_parameter(
+        Name='django-helloworld'
+    )
+    print(response)
+
+
+if __name__ == "__main__":
+    docker_client=docker.from_env() 
+    ssm_fetch_para_response=fetch_parameter()
