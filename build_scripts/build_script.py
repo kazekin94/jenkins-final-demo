10b61631c1a04d4ce82a78e8acb2050f28e1c8c5
Ankit <ankit.thakur@juspay.in>
2020-05-01 09:42:17 +0530

    testing build image

--- build_scripts/build_script.py
+++ build_scripts/build_script.py
@@ -45,8 +45,8 @@ def tag_image(client, image_name, para):
         image_tag=image_tag_template.replace('<aws_account_id>', para['aws_account_id']).replace('<aws_region>', para['aws_region']).replace('<image_name>', image_name)
         print("Old image name:", image_name)
         print("Tag to be given to image:", image_tag)
-        low_level_client=docker.APIClient(base_url='unix://var/run/docker.sock')
-        
+        low_level_client=docker.APIClient(base_url='unix://var/run/docker.sock') #set low level client
+        low_level_client.tag(image_name, image_tag.split(':')[0], image_tag, force=True)
         '''
         image_tag_response=client.tag(image_name,  tag=image_tag)     
         print("Tagging done?", image_tag_response)
