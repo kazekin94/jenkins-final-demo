42ea81cbed8d82171e3811622f472a367d8ea6e6
Ankit <ankit.thakur@juspay.in>
2020-05-01 11:55:38 +0530

    testing build image

--- build_scripts/build_script.py
+++ build_scripts/build_script.py
@@ -28,32 +28,22 @@ def build_image(client, para, path_workspace):
     docker_file_path=os.path.dirname(docker_file) # os path
     #build repo name
     repo_name_template=para['image_repo_name']
-    repo_name=repo_name_template.replace('<aws_account_id>', para['aws_account_id']).replace('<aws_region>', para['aws_region'])
-    image_name=repo_name+'/'+para['image_name']+':'+para['image_tag']
+    repo_name=repo_name_template.replace('<aws_account_id>', para['aws_account_id']).replace('<aws_region>', para['aws_region'])+'/'+para['image_name']
+    image_name=para['image_tag']
+    print(image_name)
     #build image
     print("Start building image.")
     try:
         image_build_response=client.images.build(path=docker_file_path, tag=image_name, dockerfile='Dockerfile') #returns image class obj, generator of json decoded logs
+        print("Image built:", image_build_response)
         return image_build_response
     except Exception as e:
         print("Exception in building image:", e)
 
 
-#tag image
-def tag_image(client, image_name, para, image_id):
-    try:
-        image_tag_template=para['image_tag']
-        image_tag=image_tag_template.replace('<aws_account_id>', para['aws_account_id']).replace('<aws_region>', para['aws_region']).replace('<image_name>', image_name)
-        print("Old image name:", image_name)
-        print("Tag to be given to image:", image_tag)
-        low_level_client=docker.APIClient(base_url='unix://var/run/docker.sock') #set low level client
-        
-        '''
-        image_tag_response=client.tag(image_name,  tag=image_tag)     
-        print("Tagging done?", image_tag_response)
-        '''
-    except Exception as e:
-        print("Exception in tagging:", e)       
+#push image to ecr
+def push_image(client, para, image_obj):
+    print(image_obj[0].tags)
 
 
 if __name__ == "__main__":
@@ -65,5 +55,4 @@ if __name__ == "__main__":
     #calls 
     fetch_para_response=fetch_parameter(para_name) #fetch para
     image_object=build_image(docker_client, fetch_para_response, work_space_path) #build image
-    print(image_object)
-    #call_tag_image=tag_image(docker_client, image_tag[0], fetch_para_response, image_id)
+    call_push_image=push_image(docker_client, fetch_para_response, image_object)
