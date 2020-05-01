4d42ee1d576da0cbb22f88a356a425ea1dd5e0df
Ankit <ankit.thakur@juspay.in>
2020-05-01 06:31:28 +0530

    added build image

--- build_scripts/build_script.py
+++ build_scripts/build_script.py
@@ -28,8 +28,7 @@ def build_image(client, para, path_workspace):
     docker_file_path=os.path.dirname(docker_file) # os path
     print("Path to workspace:", docker_file_path)
     image_build_response=client.images.build(path=docker_file_path, tag=para['image_name'], dockerfile='Dockerfile')
-    image_id=image_build_response.id()
-    print(image_id)
+    print(image_build_response)
 
 
 if __name__ == "__main__":
