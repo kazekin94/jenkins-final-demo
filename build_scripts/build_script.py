c6b92954b7941b0e0596bbcf3c28fcad3d6d575c
Ankit <ankit.thakur@juspay.in>
2020-05-01 11:33:50 +0530

    testing build image

--- build_scripts/build_script.py
+++ build_scripts/build_script.py
@@ -8,6 +8,7 @@ from io import BytesIO
 #fetch parameters from ssm
 def fetch_parameter(para):
     #set ssm client
+    print("Fetching paras from parameter store.")
     ssm_client=boto3.client('ssm', region_name='ap-south-1')
 
     response = ssm_client.get_parameter(
@@ -15,27 +16,27 @@ def fetch_parameter(para):
     )
     para_value=response['Parameter']['Value']
     para_value_dict=json.loads(para_value)
+    print("Parameters fetched:", para_value_dict)
     return para_value_dict
 
 
 #build image
 def build_image(client, para, path_workspace):
-    print("Fetched para:", para)
+    #build paths
     actual_path=path_workspace.replace('<pipeline_name>', para['pipeline_name']) #path to docker file
-    print("Created path to workspace:", actual_path)
     docker_file=actual_path+"/Dockerfile" #path of dockerfile
-    print("Path to dockerfile:", docker_file)
     docker_file_path=os.path.dirname(docker_file) # os path
-    print("Path to docker build context:", docker_file_path)
+    #build repo name
+    repo_name_template=para['image_repo_name']
+    repo_name=repo_name_template.replace('<aws_account_id>', para['aws_account_id']).replace('<aws_region>', para['aws_region'])
+    image_name=repo_name+'/'+para['image_name']+':'+para['image_tag']
+    #build image
     print("Start building image.")
     try:
-        image_build_response=client.images.build(path=docker_file_path, tag=para['image_name'], dockerfile='Dockerfile') #returns image class obj, generator of json decoded logs
-        image_id=image_build_response[0].id
-        image_tags=image_build_response[0].tags
-        print("Image id:", image_id, "Image tags:", image_tags)
+        image_build_response=client.images.build(path=docker_file_path, tag=image_name, dockerfile='Dockerfile') #returns image class obj, generator of json decoded logs
+        return image_build_response
     except Exception as e:
         print("Exception in building image:", e)
-    return image_id, image_tags
 
 
 #tag image
@@ -46,7 +47,7 @@ def tag_image(client, image_name, para, image_id):
         print("Old image name:", image_name)
         print("Tag to be given to image:", image_tag)
         low_level_client=docker.APIClient(base_url='unix://var/run/docker.sock') #set low level client
-        low_level_client.tag(image_id, image_tag.split(':')[0], image_tag, force=True)
+        
         '''
         image_tag_response=client.tag(image_name,  tag=image_tag)     
         print("Tagging done?", image_tag_response)
@@ -63,5 +64,5 @@ if __name__ == "__main__":
     docker_client=docker.from_env()
     #calls 
     fetch_para_response=fetch_parameter(para_name) #fetch para
-    image_id, image_tag=build_image(docker_client, fetch_para_response, work_space_path) #build image
-    call_tag_image=tag_image(docker_client, image_tag[0], fetch_para_response, image_id)
+    image_object=build_image(docker_client, fetch_para_response, work_space_path) #build image
+    #call_tag_image=tag_image(docker_client, image_tag[0], fetch_para_response, image_id)
