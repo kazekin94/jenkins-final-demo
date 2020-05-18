efe68449bf377d00c61693c4536c0b8a0a56be8d
Ankit <ankit.thakur@juspay.in>
2020-05-19 01:01:44 +0530

    dd

--- build_scripts/run_container.py
+++ build_scripts/run_container.py
@@ -32,8 +32,9 @@ def fetch_parameter(para):
 def run_container(client, root_para, deployment_para):
     ecr_client=boto3.client('ecr', region_name='ap-south-1')
     try:
+        root_para_image_name=root_para['image_name']
         current_context={}
-        [current_context.update(context) for context in deployment_para if context['image_name']==image_name]
+        [current_context.update(context) for context in deployment_para if context['image_name']==root_para_image_name]
         image_name_template=root_para['image_repo_name'].replace("<aws_account_id>", root_para['aws_account_id']).replace("<aws_region>", root_para['aws_region'])+'/'+root_para['image_name']
         auth_resp=ecr_client.get_authorization_token() #get ecr temp auth tokern
         user, passwd=base64.b64decode(auth_resp['authorizationData'][0]['authorizationToken']).decode().split(':')
